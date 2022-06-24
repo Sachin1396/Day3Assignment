2c83c993f61e31e9e3b754024799105760e1eb25
Sachin1396 <sachhushetty1396@gmail.com>
2022-06-24 18:44:35 +0530

    Solving prgram by using switch

--- EmployeePresent.java
+++ EmployeePresent.java
@@ -1,26 +1,27 @@
-public class EmployeePresent{
-    public static void main(String[] args){
+public class EmployeePresent {
+    public static void main(String[] args) {
         System.out.println("Welcome to employee wage program on master branch");
-        int part_time = 1 ;
-        int full_time = 2 ;
-        int rate_per_hour = 20 ;
+
+        int rate_per_hour = 20;
         int empHour = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck==part_time)
-        {
-            System.out.println("Employee is part time");
-            empHour=4;
-        } else if (empCheck == full_time ){
-            System.out.println("Employee is present");
-            empHour = 8;
-        }
-        else {
-            System.out.println("Employee is absent");
-            empHour = 0;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case 1:
+                empHour = 4;
+                break;
+            case 2:
+                empHour = 8;
+                break;
+            case 3:
+                empHour = 0;
+                break;
+            default:
+                System.out.println("error");
         }
         empWage = empHour * rate_per_hour;
-        System.out.println("Employee wage"+empWage);
+        System.out.println("Employee wage" + empWage);
+
     }
 }
 
