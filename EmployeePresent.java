8a9d66e5d1682627fcb2de469a94f80c42c616c0
Sachin1396 <sachhushetty1396@gmail.com>
2022-06-24 19:43:39 +0530

    Adding the monthlywage program

--- EmployeePresent.java
+++ EmployeePresent.java
@@ -5,21 +5,24 @@ public class EmployeePresent {
         int rate_per_hour = 20;
         int empHour = 0;
         int empWage = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
-            case 1:
-                empHour = 4;
-                break;
-            case 2:
-                empHour = 8;
-                break;
-            case 3:
-                empHour = 0;
-                break;
-            default:
-                System.out.println("error");
+        int number_days = 20;
+        for(int i=0;i<number_days;i++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case 1:
+                    empHour = 4;
+                    break;
+                case 2:
+                    empHour = 8;
+                    break;
+                case 3:
+                    empHour = 0;
+                    break;
+                default:
+                    System.out.println("error");
+            }
+            empWage = empWage + empHour * rate_per_hour;
         }
-        empWage = empHour * rate_per_hour;
         System.out.println("Employee wage" + empWage);
 
     }
