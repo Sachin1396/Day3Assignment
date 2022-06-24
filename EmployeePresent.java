663081852a1e82a2361a8e6d93ed9f8879cb1bde
Sachin1396 <sachhushetty1396@gmail.com>
2022-06-24 18:16:42 +0530

    Adding employee present or absent code

--- EmployeePresent.java
+++ EmployeePresent.java
@@ -1,6 +1,13 @@
 public class EmployeePresent{
     public static void main(String[] args){
         System.out.println("Welcome to employee wage program on master branch");
+        int full_time = 1 ;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == full_time ){
+            System.out.println("employee is present");
+        }
+        else
+            System.out.println("Employee is absent");
     }
 }
 
