1a194bf81af6d7ab3a81d69c1d9fd4c470ed29f5
Sachin1396 <sachhushetty1396@gmail.com>
2022-06-24 18:33:10 +0530

    Adding employee part time code

--- EmployeePresent.java
+++ EmployeePresent.java
@@ -1,12 +1,17 @@
 public class EmployeePresent{
     public static void main(String[] args){
         System.out.println("Welcome to employee wage program on master branch");
-        int full_time = 1 ;
+        int part_time = 1 ;
+        int full_time = 2 ;
         int rate_per_hour = 20 ;
         int empHour = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == full_time ){
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck==part_time)
+        {
+            System.out.println("Employee is part time");
+            empHour=4;
+        } else if (empCheck == full_time ){
             System.out.println("Employee is present");
             empHour = 8;
         }
